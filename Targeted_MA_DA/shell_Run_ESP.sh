06dc25d17067cfb726003c879d2a1d2cd51a77db
Bluejee <balakrishnaprabhu1999@gmail.com>
2025-08-05 17:34:28 +0200

    Corrected the figure number in the comments

--- Targeted_MA_DA/shell_Run_ESP.sh
+++ Targeted_MA_DA/shell_Run_ESP.sh
@@ -1,4 +1,4 @@
-# To Recreate the figure 4.1 in the paper, run this script.
+# To Recreate the figure 3.1 in the paper, run this script.
 alphas=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
 betas="[0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]"
 
