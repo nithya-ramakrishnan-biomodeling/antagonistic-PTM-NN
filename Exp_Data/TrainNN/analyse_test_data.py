7582829cb0f616a7b381429bd6d77584c59063a1
Bluejee <balakrishnaprabhu1999@gmail.com>
2025-11-03 12:21:32 +0100

    Added plotting code and updated the run to include only needed time steps and data

--- Exp_Data/TrainNN/analyse_test_data.py
+++ Exp_Data/TrainNN/analyse_test_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load test data
 test_sequences_with_corrections = pd.read_pickle('test_sequences_with_corrections.pkl')
@@ -29,3 +31,30 @@ mean_biterror_df.to_csv('Test_Data_BitError_Combinations.csv', index=False)
 
 # Run the incomplete batch matrix generator using os
 os.system('python ../../batch_matrix_incomplete.py Test_Data')
+
+#alpha beta pairs to plot
+# alpha_beta_pairs = []
+# for a in [0.6,0.7,0.8]:
+#     for b in [0.6,0.7,0.8]:
+#         if not (a == 0.6 and b == 0.6):  # Exclude the (0.6, 0.6) pair
+#             alpha_beta_pairs.append((a, b))
+
+alpha_beta_pairs = [(0.6, 0.7), (0.6, 0.8), (0.7, 0.6), (0.7, 0.7), (0.7, 0.8)]
+
+# Filter the data for the selected alpha-beta pairs
+filtered_data = test_sequences_with_corrections[
+    test_sequences_with_corrections[['alpha', 'beta']].apply(tuple, axis=1).isin(alpha_beta_pairs)
+]
+
+# Create a new column for the alpha-beta pair as a string
+filtered_data['Alpha-Beta Pair'] = filtered_data.apply(lambda row: f"({row['alpha']}, {row['beta']})", axis=1)
+
+# Plot violin plot with alpha-beta pair as x-axis
+plt.figure(figsize=(10, 6))
+sns.violinplot(x='Alpha-Beta Pair', y='biterror', data=filtered_data, palette='muted')
+plt.title('BitError Distribution for Selected Alpha-Beta Pairs')
+plt.ylabel('BitError')
+plt.xlabel('Alpha-Beta Pair')
+plt.tight_layout()
+plt.savefig('BitError_Violin_AlphaBeta_Pairs.png')
+plt.close()
--- Exp_Data/TrainNN/exp_data_trainNN.py
+++ Exp_Data/TrainNN/exp_data_trainNN.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
         modifications = ['H2AK5ac', 'H2AS129ph', 'H3K14ac', 'H3K18ac', 'H3K23ac', 'H3K27ac', 'H3K36me', 'H3K36me2', 'H3K36me3',
                         'H3K4ac', 'H3K4me', 'H3K4me2', 'H3K4me3', 'H3K56ac', 'H3K79me', 'H3K79me3', 'H3K9ac', 'H3S10ph',
                         'H4K12ac', 'H4K16ac', 'H4K20me', 'H4K5ac', 'H4K8ac', 'H4R3me', 'H4R3me2s', 'Htz1']
-        modification_time_list = ['t0', 't4 ', 't8', 't15', 't30', 't60']
+        modification_time_list = ['t0']# , 't4', 't8', 't15', 't30', 't60']
         # modification_time_list = ['t0', 't4']
 
         all_sequences_list = []
